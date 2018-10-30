770a9c93185af27939f32b993c2231920921d15e
Mikkel Munk <mmunk@toogoodtogo.com>
2018-10-30 16:41:05 +0100

    Add stacktrace and add function

--- calculator.js
+++ calculator.js
@@ -1,7 +1,26 @@
+var inputCounter = 0;
+
 function add() {
-    console.log('add');
+    inputCounter++;
+    var inputValue = getElementValue('input')
+    var stackValue = getElementValue('stack');
+
+    if (!stackValue) {
+        getElement('stack').value += inputValue;
+        return;
+    }
+
+    getElement('stack').value += ' + ' + inputValue;
 }
 
 function calculate() {
     console.log('calculate');
+}
+
+function getElementValue(id) {
+    return document.getElementById(id).value;
+}
+
+function getElement(id) {
+    return document.getElementById(id);
 }
