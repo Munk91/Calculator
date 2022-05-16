5a8c7658e528d792ca0e772ae2b6ccf012c6accd
Mikkel Bendix Munk <mimu@tv2.dk>
2022-05-16 18:28:58 +0200

    WIP Basic calculator

--- js/BasicCalculator/BasicCalculator.js
+++ js/BasicCalculator/BasicCalculator.js
@@ -4,50 +4,61 @@ var result = 0;
 function input() {
   var inputValue = getElementValue("input");
   var stackValue = getElementValue("stack");
+  var inputElement = getElement("input");
+  var stackElement = getElement("stack");
+
+  if (inputValue) {
+    stackElement.value += inputValue;
+  }
 }
 
-function add() {
+function addToStack(operator) {
   var inputValue = getElementValue("input");
   var stackValue = getElementValue("stack");
 
-  inputValue = parseInt(inputValue);
-  if (isNaN(inputValue)) {
+  var stackValue;
+
+  if (!inputValue) {
     return;
   }
 
-  if (!stackValue) {
-    getElement("stack").value += inputValue;
-  } else {
-    getElement("stack").value += " + " + inputValue;
-  }
-
-  result += inputValue;
-  getElement("input").value = "";
-  getElement("input").focus();
-  inputCounter++;
-}
-
-function subtract() {
-  var inputValue = getElementValue("input");
-  var stackValue = getElementValue("stack");
-
-  inputValue = parseInt(inputValue);
-  if (isNaN(inputValue)) {
-    return;
-  }
+  stackValue = inputValue + ' ' + operator;
 
   if (!stackValue) {
-    getElement("stack").value += inputValue;
+    getElement("stack").value += stackValue;
   } else {
-    getElement("stack").value += " - " + inputValue;
+    getElement("stack").value += " " + stackValue;
   }
 
-  result -= inputValue;
-  getElement("input").value = "";
-  getElement("input").focus();
-  inputCounter++;
+  clearElementValue("input");
 }
 
 function calculate() {
-  getElement("result").value = result;
+  // str split on space
+  // if operator is +
+  //   result += input
+  // else if operator is -
+  //   result -= input
+  // else if operator is *
+  //   result *= input
+  // else if operator is /
+  //   result /= input
+  // else if operator is %
+  //   result %= input
+  // else if operator is ^
+  //   result ^= input
+  // else if operator is =
+  // else if operator is !
+  // else if operator is (
+  // else if operator is )
+  // else if operator is sqrt
+  // else if operator is sin
+  // else if operator is cos
+  // else if operator is tan
+  // else if operator is log
+  // else if operator is ln
+  // else if operator is pi
+  // else if operator is e
+
+  getElement("result").value = 'WORK IN PROGRESS';
 }
--- js/helpers.js
+++ js/helpers.js
@@ -2,6 +2,10 @@ function getElementValue(id) {
   return document.getElementById(id).value;
 }
 
+function clearElementValue(id) {
+  document.getElementById(id).value = null;
+}
+
 function getElement(id) {
   return document.getElementById(id);
 }
